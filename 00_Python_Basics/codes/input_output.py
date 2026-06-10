a169e01bacd5554ef7609b8ba9c83d15d205ddc4
noorgupta <noorgupta24@navgurukul.org>
2026-06-10 15:52:50 +0530

    Done with learning how to take input and output

---
+++ 00_Python_Basics/codes/input_output.py
@@ -0,0 +1,20 @@
+# 1. taking input 
+
+name = input("Enter your name: ")
+print(name)
+
+# 2. Taking input but converting input's datatype in integer 
+
+n = int(input("Enter any random number: "))
+print(n)
+print(type(n))
+
+# 3. taking multiple inputs 
+
+a, b = map(int, input("Enter any two random numbers: ").split())
+print(a + b)
+
+# 4. Taking input and putting it into a list(array)
+
+arr = list(map(int, input().split()))
+print(arr)
