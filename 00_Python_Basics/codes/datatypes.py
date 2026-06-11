0043e71522c8598e47c0879bcb5cdd5ef0075274
noorgupta <noorgupta24@navgurukul.org>
2026-06-11 11:10:11 +0530

    Added the datatypes's  notes and it's coding material

---
+++ 00_Python_Basics/codes/datatypes.py
@@ -0,0 +1,22 @@
+# Integer
+a = 10
+
+# Float
+b = 10.5
+
+# String
+c = "10"
+
+# Boolean
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(int("100"))
+print(float("99.99"))
+print(str(50))
+print(bool(1))
+print(bool(0))
