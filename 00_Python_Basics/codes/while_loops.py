9b2ed349b436da2c475d57db19e8c7db60536775
noorgupta <noorgupta24@navgurukul.org>
2026-07-08 15:44:11 +0530

    added the code for while loops

---
+++ 00_Python_Basics/codes/while_loops.py
@@ -0,0 +1,29 @@
+# Example 1
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+print("-" * 30)
+
+# Example 2
+
+num = 2
+
+while num <= 10:
+    print(num)
+    num += 2
+
+print("-" * 30)
+
+# Example 3
+
+count = 5
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("Blast Off!")
