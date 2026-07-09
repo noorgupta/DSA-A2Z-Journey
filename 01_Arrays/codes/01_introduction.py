67c6043cd372d2321178f72ecb173bab5f14360b
noorgupta <noorgupta24@navgurukul.org>
2026-07-09 12:39:52 +0530

    added the code example of arrys that how to print full array, it's length, it's elements per index, and how to modify the element

---
+++ 01_Arrays/codes/01_introduction.py
@@ -0,0 +1,29 @@
+# Creating an array (Python List)
+
+marks = [85, 92, 78, 95, 88]
+
+print("Complete Array:")
+print(marks)
+
+print("-" * 30)
+
+# Accessing elements using index
+
+print("First Element:", marks[0])
+print("Third Element:", marks[2])
+print("Last Element:", marks[4])
+
+print("-" * 30)
+
+# Length of the array
+
+print("Length:", len(marks))
+
+print("-" * 30)
+
+# Updating an element
+
+marks[2] = 80
+
+print("Updated Array:")
+print(marks)
