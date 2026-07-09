7eb786941e6ab996b247db5978307582ba4060b6
noorgupta <noorgupta24@navgurukul.org>
2026-07-09 11:21:28 +0530

    added the code examples for time complexity that how for each question or code we need to find time complexity

---
+++ 00_Python_Basics/codes/time_complexity.py
@@ -0,0 +1,19 @@
+# O(1)
+
+x = 10
+print(x)
+
+print("-" * 30)
+
+# O(n)
+
+for i in range(5):
+    print(i)
+
+print("-" * 30)
+
+# O(n²)
+
+for i in range(3):
+    for j in range(3):
+        print(i, j)
