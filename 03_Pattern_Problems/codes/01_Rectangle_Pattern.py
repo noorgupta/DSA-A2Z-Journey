bfb1ea9271ed26dcd9d0b0d782e8c98da097944c
noorgupta <noorgupta24@navgurukul.org>
2026-07-13 15:14:28 +0530

    altered the file of Reactangle pattern question and added the observation

--- 03_Pattern_Problems/codes/01_Rectangle_Pattern.py
+++ 03_Pattern_Problems/codes/01_Rectangle_Pattern.py
@@ -10,8 +10,46 @@ Pattern 1
 Time Complexity: O(n²)
 Space Complexity: O(1)
 """
+##Code 
 
 for rows in range(5):
     for columns in range(5):
         print("*", end="") # Print on same line
     print()                # Move to next line
+
+
+## Observation
+
+Rows = 5
+
+Columns = 5
+
+Outer Loop → Rows
+
+Inner Loop → Columns
+
+---
+
+## Logic
+
+(Short explanation after you solve it.)
+
+---
+
+## Time Complexity
+
+O(n²)
+
+---
+
+## Space Complexity
+
+O(1)
+
+---
+
+## Learning
+
+- Learned nested loops.
+- Learned `print(..., end="")`.
+- Learned the purpose of `print()`.
